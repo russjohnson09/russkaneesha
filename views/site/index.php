77b76fe2edf2f12d4f398dcc85c1a6a324abe8c8
Russell Johnson II <russell@interservsystems.com>
2015-11-10 16:39:05 -0500

    removed extra

--- views/site/index.php
+++ views/site/index.php
@@ -15,7 +15,6 @@ $app = Yii::$app;
 ?>
 <div class="site-index">
 
-    <div class="body-content">
         <?php if(!isset($guest)):?>
             <?php $form = ActiveForm::begin([
                 'id' => 'log-form',
@@ -38,61 +37,5 @@ $app = Yii::$app;
 
             <?php ActiveForm::end(); ?>
         <?php endif;?>
-        <div class="body-content">
 
-            <?php $form = ActiveForm::begin([
-                'id' => 'guest-login-form',
-                'options' => ['class' => 'form-horizontal'],
-                'fieldConfig' => [
-                    //'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-                    'labelOptions' => ['class' => 'col-lg-1 control-label'],
-                ],
-            ]); ?>
-
-            <?php $form->field($guest, 'first_name') ?>
-
-            <?php $form->field($guest, 'last_name') ?>
-
-            <div class="form-group">
-                <div class="col-lg-offset-1 col-lg-11">
-                    <?php Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            </div>
-
-            <?php ActiveForm::end(); ?>
-
-            <div class="row">
-                <div class="col-lg-4">
-                    <h2>Heading</h2>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur.</p>
-
-                    <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-                </div>
-                <div class="col-lg-4">
-                    <h2>Heading</h2>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur.</p>
-
-                    <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-                </div>
-                <div class="col-lg-4">
-                    <h2>Heading</h2>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur.</p>
-
-                    <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-                </div>
-            </div>
-
-        </div>
-    </div>
+</div>
